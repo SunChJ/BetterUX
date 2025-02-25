8027b93cd4c27a16de173afcaec44c1de76df008
SamsonCJ <CS3507529000@gmail.com>
2025-02-25 21:18:58 +0800

    chore: project init

---
+++ InfiniteScrollView/InfiniteScrollView/InfiniteScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollViewApp.swift
+//  InfiniteScrollView
+//
+//  Created by SamsonCJ on 2025/2/25.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InfiniteScrollView/InfiniteScrollViewTests/InfiniteScrollViewTests.swift
@@ -0,0 +1,16 @@
+//
+//  InfiniteScrollViewTests.swift
+//  InfiniteScrollViewTests
+//
+//  Created by SamsonCJ on 2025/2/25.
+//
+
+import Testing
+
+struct InfiniteScrollViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
